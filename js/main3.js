b79bc596f3b048c814a96aad0b9547b108641fb1
George Joseph <georgejoseph5000@gmail.com>
2021-12-15 03:24:42 +0200

    first commit

---
+++ js/main3.js
@@ -0,0 +1,45 @@
+function changeColor(id) {
+    setInterval(() => {
+        if (document.getElementById(id).style.backgroundColor == "white") {
+            document.getElementById(id).style.backgroundColor = "green";
+        } else if (document.getElementById(id).style.backgroundColor == "green") {
+            document.getElementById(id).style.backgroundColor = "blue";
+        } else if (document.getElementById(id).style.backgroundColor == "blue") {
+            document.getElementById(id).style.backgroundColor = "violet";
+        } else if (document.getElementById(id).style.backgroundColor == "violet") {
+            document.getElementById(id).style.backgroundColor = "red";
+        } else if (document.getElementById(id).style.backgroundColor == "red") {
+            document.getElementById(id).style.backgroundColor = "black";
+        }else if(document.getElementById(id).style.backgroundColor == "black"){
+            document.getElementById(id).style.backgroundColor = "white";
+        }
+    }, 500);
+    document.getElementById(id).style.backgroundColor = "black";
+}
+changeColor("colors");
+
+
+setInterval(setScroll,200);
+
+function setScroll() {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            document.getElementById("main-nav").style.backgroundColor = "red";
+            document.getElementById("dropdown-menu1").style.backgroundColor = "red";
+            
+        }else{
+            document.getElementById("main-nav").style.backgroundColor = "";
+            document.getElementById("dropdown-menu1").style.backgroundColor = "rgb(106, 106, 106)";
+            
+        }
+    });
+}
+
+
+
+
+//if(document.)
+
+
+
+//border-right: 2px solid black;
